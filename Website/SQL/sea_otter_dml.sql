596690f0523bf1824a6558d8666ef562f62ffc4d
Hau'olinalani O'Brien <147751457+hauolinalani@users.noreply.github.com>
2025-05-07 19:29:32 -0700

    Update sea_otter_dml.sql

--- Website/SQL/sea_otter_dml.sql
+++ Website/SQL/sea_otter_dml.sql
@@ -1,1 +1,17 @@
+-- INSERT CUSTOMER
+INSERT INTO Customers (Name, Species, Address, ContactEmail)
+VALUES (@name, @species, @address, @contactEmail);
+
+-- SELECT ALL PRODUCTS
+SELECT * FROM Products;
+
+-- INSERT PRODUCT
+INSERT INTO Products (Name, Item, PricePerUnit, Quantity, SupplierID)
+VALUES (@name, @item, @price, @quantity, @supplierID);
+
+-- UPDATE ORDER STATUS
+UPDATE Orders SET OrderStatus = @status WHERE OrderID = @orderID;
+
+-- DELETE REVIEW
+DELETE FROM Reviews WHERE ReviewID = @reviewID;
 
